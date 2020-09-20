1ecd262dc3bdbe96cfdaead0c14f6e5e07d3a179
Andoni Eguíluz <andoni.eguiluz@deusto.es>
2020-09-20 20:15:34 +0200

    Test

--- ProgIII-Practicas/src/ud/prog3/pr01/ListaDeReproduccion.java
+++ ProgIII-Practicas/src/ud/prog3/pr01/ListaDeReproduccion.java
@@ -7,104 +7,105 @@ import javax.swing.event.ListDataEvent;
 import javax.swing.event.ListDataListener;
 
 
-/** Clase para crear instancias como listas de reproducción,
- * que permite almacenar listas de ficheros con posición de índice
+/** Clase para crear instancias como listas de reproducciï¿½n,
+ * que permite almacenar listas de ficheros con posiciï¿½n de ï¿½ndice
  * (al estilo de un array / arraylist)
- * con marcas de error en los ficheros y con métodos para cambiar la posición
- * de los elementos en la lista, borrar elementos y añadir nuevos.
+ * con marcas de error en los ficheros y con mï¿½todos para cambiar la posiciï¿½n
+ * de los elementos en la lista, borrar elementos y aï¿½adir nuevos.
  */
 public class ListaDeReproduccion implements ListModel<String> {
-	ArrayList<File> ficherosLista;     // ficheros de la lista de reproducción
+	ArrayList<File> ficherosLista;     // ficheros de la lista de reproducciï¿½n
 	int ficheroEnCurso = -1;           // Fichero seleccionado (-1 si no hay ninguno seleccionado)
+	// Test
 	
 	/** Devuelve uno de los ficheros de la lista
-	 * @param posi	Posición del fichero en la lista (de 0 a size()-1)
-	 * @return	Devuelve el fichero en esa posición
-	 * @throws IndexOutOfBoundsException	Si el índice no es válido
+	 * @param posi	Posiciï¿½n del fichero en la lista (de 0 a size()-1)
+	 * @return	Devuelve el fichero en esa posiciï¿½n
+	 * @throws IndexOutOfBoundsException	Si el ï¿½ndice no es vï¿½lido
 	 */
 	public File getFic( int posi ) throws IndexOutOfBoundsException {
 		return ficherosLista.get( posi );
 	}	
 
-	/** Añade a la lista de reproducción todos los ficheros que haya en la 
+	/** Aï¿½ade a la lista de reproducciï¿½n todos los ficheros que haya en la 
 	 * carpeta indicada, que cumplan el filtro indicado.
-	 * Si hay cualquier error, la lista de reproducción queda solo con los ficheros
+	 * Si hay cualquier error, la lista de reproducciï¿½n queda solo con los ficheros
 	 * que hayan podido ser cargados de forma correcta.
 	 * @param carpetaFicheros	Path de la carpeta donde buscar los ficheros
 	 * @param filtroFicheros	Filtro del formato que tienen que tener los nombres de
 	 * 							los ficheros para ser cargados.
-	 * 							String con cualquier letra o dígito. Si tiene un asterisco
-	 * 							hace referencia a cualquier conjunto de letras o dígitos.
+	 * 							String con cualquier letra o dï¿½gito. Si tiene un asterisco
+	 * 							hace referencia a cualquier conjunto de letras o dï¿½gitos.
 	 * 							Por ejemplo p*.* hace referencia a cualquier fichero de nombre
-	 * 							que empiece por p y tenga cualquier extensión.
-	 * @return	Número de ficheros que han sido añadidos a la lista
+	 * 							que empiece por p y tenga cualquier extensiï¿½n.
+	 * @return	Nï¿½mero de ficheros que han sido aï¿½adidos a la lista
 	 */
 	public int add(String carpetaFicheros, String filtroFicheros) {
-		// TODO: Codificar este método de acuerdo a la práctica (pasos 3 y sucesivos)
-		filtroFicheros = filtroFicheros.replaceAll( "\\.", "\\\\." );  // Pone el símbolo de la expresión regular \. donde figure un .
+		// TODO: Codificar este mï¿½todo de acuerdo a la prï¿½ctica (pasos 3 y sucesivos)
+		filtroFicheros = filtroFicheros.replaceAll( "\\.", "\\\\." );  // Pone el sï¿½mbolo de la expresiï¿½n regular \. donde figure un .
 		return 0;
 	}
 	
 	
 	//
-	// Métodos de selección
+	// Mï¿½todos de selecciï¿½n
 	//
 	
-	/** Seleciona el primer fichero de la lista de reproducción
-	 * @return	true si la selección es correcta, false si hay error y no se puede seleccionar
+	/** Seleciona el primer fichero de la lista de reproducciï¿½n
+	 * @return	true si la selecciï¿½n es correcta, false si hay error y no se puede seleccionar
 	 */
 	public boolean irAPrimero() {
 		ficheroEnCurso = 0;  // Inicia
 		if (ficheroEnCurso>=ficherosLista.size()) {
-			ficheroEnCurso = -1;  // Si no se encuentra, no hay selección
+			ficheroEnCurso = -1;  // Si no se encuentra, no hay selecciï¿½n
 			return false;  // Y devuelve error
 		}
 		return true;
 	}
 	
-	/** Seleciona el último fichero de la lista de reproducción
-	 * @return	true si la selección es correcta, false si hay error y no se puede seleccionar
+	/** Seleciona el ï¿½ltimo fichero de la lista de reproducciï¿½n
+	 * @return	true si la selecciï¿½n es correcta, false si hay error y no se puede seleccionar
 	 */
 	public boolean irAUltimo() {
 		ficheroEnCurso = ficherosLista.size()-1;  // Inicia al final
-		if (ficheroEnCurso==-1) {  // Si no se encuentra, no hay selección
+		if (ficheroEnCurso==-1) {  // Si no se encuentra, no hay selecciï¿½n
 			return false;  // Y devuelve error
 		}
 		return true;
 	}
 
-	/** Seleciona el anterior fichero de la lista de reproducción
-	 * @return	true si la selección es correcta, false si hay error y no se puede seleccionar
+	/** Seleciona el anterior fichero de la lista de reproducciï¿½n
+	 * @return	true si la selecciï¿½n es correcta, false si hay error y no se puede seleccionar
 	 */
 	public boolean irAAnterior() {
 		if (ficheroEnCurso>=0) ficheroEnCurso--;
-		if (ficheroEnCurso==-1) {  // Si no se encuentra, no hay selección
+		if (ficheroEnCurso==-1) {  // Si no se encuentra, no hay selecciï¿½n
 			return false;  // Y devuelve error
 		}
 		return true;
 	}
 
-	/** Seleciona el siguiente fichero de la lista de reproducción
-	 * @return	true si la selección es correcta, false si hay error y no se puede seleccionar
+	/** Seleciona el siguiente fichero de la lista de reproducciï¿½n
+	 * @return	true si la selecciï¿½n es correcta, false si hay error y no se puede seleccionar
 	 */
 	public boolean irASiguiente() {
 		ficheroEnCurso++;
 		if (ficheroEnCurso>=ficherosLista.size()) {
-			ficheroEnCurso = -1;  // Si no se encuentra, no hay selección
+			ficheroEnCurso = -1;  // Si no se encuentra, no hay selecciï¿½n
 			return false;  // Y devuelve error
 		}
 		return true;
 	}
 
 	/** Devuelve el fichero seleccionado de la lista
-	 * @return	Posición del fichero seleccionado en la lista de reproducción (0 a n-1), -1 si no lo hay
+	 * @return	Posiciï¿½n del fichero seleccionado en la lista de reproducciï¿½n (0 a n-1), -1 si no lo hay
 	 */
 	public int getFicSeleccionado() {
 		return ficheroEnCurso;
 	}
 
 	//
-	// Métodos de DefaultListModel
+	// Mï¿½todos de DefaultListModel
 	//
 	
 	@Override
@@ -129,7 +130,7 @@ public class ListaDeReproduccion implements ListModel<String> {
 		misEscuchadores.remove( l );
 	}
 	
-	// Llamar a este método cuando se añada un elemento a la lista
+	// Llamar a este mï¿½todo cuando se aï¿½ada un elemento a la lista
 	// (Utilizado para avisar a los escuchadores de cambio de datos de la lista)
 	private void avisarAnyadido( int posi ) {
 		for (ListDataListener ldl : misEscuchadores) {
